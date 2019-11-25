6f6510ba2158a81e7467f5cbfb31e0d5610b1b30
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-11-25 03:05:44 +0300

    gcd func

---
+++ numbers/gcd.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+
+int gcd(int a, int b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+int main() {
+    int a;
+    int b;
+    
+    cin >> a >> b;
+    
+    cout << gcd(a, b) << endl;
+    
+    return 0;
+}

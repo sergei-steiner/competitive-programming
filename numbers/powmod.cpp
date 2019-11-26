83b669571da0941909e385cf6364e5c70ca39944
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-11-26 04:57:44 +0300

    mod

--- numbers/powmod.cpp
+++ numbers/powmod.cpp
@@ -9,7 +9,9 @@ typedef long long int64;
 
 using namespace std;
 
-int64 powmod(int64 a, int64 n, int64 p) {
+consexpr int64 mod = 1000000007;
+
+int64 powmod(int64 a, int64 n, int64 p = mod) {
     if (n == 0) return 1 % p;
     if (n % 2 == 1) return a * powmod(a, n - 1, p) % p;
     int64 x = powmod(a, n / 2, p);

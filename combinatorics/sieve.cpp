42b8d1c6a4148194e42e5ddeea2aa12afdae1506
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-11-27 22:02:21 +0300

    style

--- combinatorics/sieve.cpp
+++ combinatorics/sieve.cpp
@@ -13,10 +13,11 @@ vector<bool> sieve(int n) {
     vector<bool> prime(n + 1, true);
     prime[0] = false;
     prime[1] = false;
-    for (int i = 2; i * i <= n; ++i)
-	    if (prime[i])
-   		    for (int j = i * i; j <= n; j += i)
-	            prime[j] = false;
+    for (int i = 2; i * i <= n; ++i) {
+	    if (prime[i]) {
+   		    for (int j = i * i; j <= n; j += i) prime[j] = false;
+	    }
+    }
     return prime;
 }
 

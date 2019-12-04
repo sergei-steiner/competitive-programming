308aa25a26e7b9881c51632aef0a30cd4ac7ff9d
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-04 23:07:15 +0300

    lcm

--- numbers/gcd.cpp
+++ numbers/gcd.cpp
@@ -14,13 +14,20 @@ int gcd(int a, int b) {
     return b ? gcd(b, a % b) : a;
 }
 
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+
+
+
 int main() {
     int a;
     int b;
     
     cin >> a >> b;
     
-    cout << gcd(a, b) << endl;
+    cout << gcd(a, b) << " " << lcm(a, b) << endl;
     
     return 0;
 }

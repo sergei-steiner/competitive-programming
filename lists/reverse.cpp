0685b9165778c2fd67303e00f4f2804d1eb3452e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-08 22:47:31 +0300

    reverse list

---
+++ lists/reverse.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode(int x)
+       : val(x)
+       , next(nullptr)
+   {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    if (head == nullptr) return nullptr;
+    ListNode* prev = nullptr;
+    ListNode* cur = head;
+    ListNode* next = head->next;
+    while (true) {
+        cur->next = prev;
+        prev = cur;
+        if (next == nullptr) return cur;
+        cur = next;
+        next = next->next;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int x;
+    cin >> x;
+    auto head = new ListNode(x);
+    auto cur;
+    
+    for (int i = 1; i < n; ++i) {
+        int x;
+        cin >> x;
+        cur->next = new ListNode(x);
+        cur = cur->next;
+    } 
+    head = reverseList(head);
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+
+    return 0;
+}

53078fde169d37fdf220e4b9a9f1f8ce98c49348
Sergey Shteiner <steiner@sandbox-407922969>
2019-12-14 00:43:18 +0300

    works for [1, n] only

--- digits/kth_lexicographic_in_range.cpp
+++ digits/kth_lexicographic_in_range.cpp
@@ -9,36 +9,36 @@ typedef long long int64;
 
 using namespace std;
 
-int64 findKthNumber(int64 left, int64 right, int64 k) {
-        int64 ans = left;
-        while (k > 1) {
-            int64 with_prefix_ans = less_with_prefix(n, ans);
-            if (with_prefix_ans < k) {
-                ++ans;
-                k -= with_prefix_ans;
-            } else {
-                ans *= 10;
-                --k;
-            }
-        }
-        return ans;
+int64 less_with_prefix(int64 n, int64 prefix) {
+    int64 ans = 0;
+    int64 left = prefix;
+    int64 right = prefix + 1;
+    while (left <= n) {
+        ans += min(right, n + 1) - left;
+        left *= 10;
+        right *= 10;
     }
-    with_prefix_ans less_with_prefix(int64 n, int64 prefix) {
-        int64 ans = 0;
-        int64 left = prefix;
-        int64 right = prefix + 1;
-        while (left <= n) {
-            ans += min(right, n + 1) - left;
-            left *= 10;
-            right *= 10;
-        }
-        return ans;
-    }
+    return ans;
+}
 
+int64 findKthNumber(int64 n, int64 k) {
+    int64 ans = 1;
+    while (k > 1) {
+        int64 with_prefix_ans = less_with_prefix(n, ans);
+        if (with_prefix_ans < k) {
+            ++ans;
+            k -= with_prefix_ans;
+        } else {
+            ans *= 10;
+            --k;
+        }
+    }
+    return ans;
+}
 
 int main() {
-    int64 left, right, k;
-    cin >> left >> right >> k;
-    cout << findKthNumber(left, right, k) << endl;
+    int64 n, k;
+    cin >> n >> k;
+    cout << findKthNumber(n, k) << endl;
     return 0;
 }

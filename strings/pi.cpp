b1ec606b9010e5b6e36dc5df2c6959a0c77e2314
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-17 17:09:23 +0300

    space

--- strings/pi.cpp
+++ strings/pi.cpp
@@ -10,15 +10,15 @@ typedef long long int64;
 using namespace std;
 
 vector<int> pi_func(const string& s) {
-	int n = sz(s);
-	vector<int> pi(n, 0);
-	for (int i = 1; i < n; ++i) {
-		int j = pi[i - 1];
-		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
-		if (s[i] == s[j]) ++j;
-		pi[i] = j;
-	}
-	return pi;
+    int n = s.size();
+    vector<int> pi(n, 0);
+    for (int i = 1; i < n; ++i) {
+        int j = pi[i - 1];
+	while (j > 0  && s[i] != s[j]) j = pi[j];
+	if (s[i] == s[j]) ++j;
+	pi[i] = j;
+    }
+    return pi;
 }
 
 int main() {

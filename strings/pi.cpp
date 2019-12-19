1ed3bd5fea6cfb2456f7851b082edbdff8068106
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-19 20:12:52 +0300

    j - 1

--- strings/pi.cpp
+++ strings/pi.cpp
@@ -14,7 +14,7 @@ vector<int> pi_func(const string& s) {
     vector<int> pi(n, 0);
     for (int i = 1; i < n; ++i) {
         int j = pi[i - 1];
-	while (j > 0  && s[i] != s[j]) j = pi[j];
+	while (j > 0  && s[i] != s[j]) j = pi[j - 1];
 	if (s[i] == s[j]) ++j;
 	pi[i] = j;
     }

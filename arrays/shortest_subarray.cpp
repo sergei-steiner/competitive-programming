4abca3a4f06e799f1edcaea1e5222fdd0800714a
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-19 20:09:23 +0300

    fix

--- arrays/shortest_subarray.cpp
+++ arrays/shortest_subarray.cpp
@@ -37,7 +37,7 @@ int main() {
     int n;
     cin >> n;
     int k;
-    cin > k;
+    cin >> k;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
     cout << ShortestSubarray(a, k) << endl;

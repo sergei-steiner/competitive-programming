6a781f5305b141287e8a691e594b7c7540d5d37a
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-23 17:21:52 +0300

    revert

--- arrays/sliding_max.cpp
+++ arrays/sliding_max.cpp
@@ -9,33 +9,147 @@ typedef long long int64;
 
 using namespace std;
 
-int majority(const vector<int>& a, int k) {
-    unordered_map<int> cands;
-    for (int x : a) {
-        ++cands[x];
-        if (cands.size() == k) {
-            for (auto it = cands.begin(); it != cands.end(); ) {
-                --it.second;
-                if (it->second == 0) {
-                    it = cands.erase(it);
-                } else {
-                    ++it;
-                }
-            }
+vector<int> max_sliding_window(vector<int>& a, int k) {
+    int n = sz(a);
+    if (n == 0) return {};
+    vector<int> prefix = a;
+    for (int i = 1; i < n; ++i) {
+        if (i % k == 0) continue;
+        prefix[i] = max(prefix[i], prefix[i - 1]);
+    }
+    vector<int> suffix = a;
+    for (int i = n - 2; i >= 0; --i) {
+        if (i % k == k - 1) continue;
+        suffix[i] = max(suffix[i], suffix[i + 1]);
+    }
+    vector<int> ans;
+    for (int i = 0; i + k <= n; ++i) {
+        ans.push_back(max(suffix[i], prefix[i + k - 1]));
+    }
+    return ans;
+}
+
+struct MaxQueue {
+    deque<pair<int, int>> d;
+    
+    void push(int val) {
+        int cnt = 1;
+        while (!d.empty() && d.back().first <= val) {
+            cnt += d.back().second;
+            d.pop_back();
+        }
+        d.emplace_back(val, cnt);
+    }
+
+    void pop() {
+        int val = d.front().first;
+        int cnt = d.front().second - 1;
+        d.pop_front();
+        if (cnt > 0) {
+            d.emplace_front(val, cnt);
         }
     }
     
-    unordered_map<int> counts;
-    for (int x : a) {
-        if (cands.count(x)) {
-            ++counts[x];
-            if (counts[x] * k > n) {
-                return x;
+    bool empty() const {
+        return d.empty();
+    }
+    
+    int max() const {
+        return d.front().first;
+    }
+};
+
+vector<int> max_sliding_window_max_queue(vector<int>& a, int k) {
+    int n = a.size();
+    if (n == 0) return {};
+    MaxQueue q;
+    for (int i = 0; i + 1 < k; ++i) {
+        q.push(a[i]);
+    }
+    vector<int> ans;
+    for (int i = k - 1; i < n; ++i) {
+        q.push(a[i]);
+        if (i >= k) q.pop();
+        ans.push_back(q.max());
+    }
+    return ans;
+}
+
+
+struct MaxStack {
+    stack<pair<int, int>> st;
+    
+    void push(int val) {
+        using std::max;
+        int maxval = val;
+        if (!st.empty()) {
+            maxval = max(maxval, st.top().second);
+        }
+        st.push({val, maxval});
+    }
+
+    int pop() {
+        int val = st.top().second;
+        st.pop();
+        return val;
+    }
+
+    bool empty() const {
+        return st.empty();
+    }
+    
+    int max() const {
+        return st.top().second;
+    }
+    
+};
+
+struct MaxQueueStacks {
+    MaxStack first;
+    MaxStack second;
+
+    void push(int val) {
+        first.push(val);
+    }
+
+    void pop() {
+        if (second.empty()) {
+            while (!first.empty()) {
+                 second.push(first.pop());
             }
         }
+        second.pop();
     }
-    return -1;
+    
+    bool empty() const {
+        return first.empty() && second.empty();
+    }
+    
+    int max() const {
+        using std::max;
+        if (first.empty()) return second.max();
+        if (second.empty()) return first.max();
+        
+        return max(first.max(), second.max());
+    }
+};
+
+vector<int> max_sliding_window_max_queue_two_stacks(vector<int>& a, int k) {
+    int n = a.size();
+    if (n == 0) return {};
+    MaxQueueStacks q;
+    for (int i = 0; i + 1 < k; ++i) {
+        q.push(a[i]);
+    }
+    vector<int> ans;
+    for (int i = k - 1; i < n; ++i) {
+        q.push(a[i]);
+        if (i >= k) q.pop();
+        ans.push_back(q.max());
+    }
+    return ans;
 }
+
 
 int main() {
     int n;
@@ -44,7 +158,14 @@ int main() {
     cin >> k;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
-    cout << majority(a, k) << endl;
-
+    for (int x : max_sliding_window(a, k)) cout << x << " ";
+    cout << endl;
+    for (int x : max_sliding_window_max_queue(a, k)) cout << x << " ";
+    cout << endl;
+    for (int x : max_sliding_window_max_queue_two_stacks(a, k)) cout << x << " ";
+    cout << endl;
+    
+    
+    
     return 0;
 }

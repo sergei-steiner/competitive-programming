4ce10ac2ef306c72ef2b3a8c7762dc50210eb3ee
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-23 17:20:06 +0300

    majority element

---
+++ arrays/majority_element.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+int majority(const vector<int>& a, int k) {
+    unordered_map<int> cands;
+    for (int x : a) {
+        ++cands[x];
+        if (cands.size() == k) {
+            for (auto it = cands.begin(); it != cands.end(); ) {
+                --it.second;
+                if (it->second == 0) {
+                    it = cands.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+    }
+    
+    unordered_map<int> counts;
+    for (int x : a) {
+        if (cands.count(x)) {
+            ++counts[x];
+            if (counts[x] * k > n) {
+                return x;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    cout << majority(a, k) << endl;
+
+    return 0;
+}

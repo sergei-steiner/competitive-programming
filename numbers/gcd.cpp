617fecd4564feef8904ae927e4825d682a66f036
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-25 20:28:09 +0300

    gcdex

--- numbers/gcd.cpp
+++ numbers/gcd.cpp
@@ -18,6 +18,19 @@ int lcm(int a, int b) {
     return a / gcd(a, b) * b;
 }
 
+int gcd (int a, int b, int& x, int& y) {
+	if (a == 0) {
+		x = 0;
+        y = 1;
+		return b;
+	}
+	int x1, y1;
+	int d = gcd (b % a, a, x1, y1);
+	x = y1 - (b / a) * x1;
+	y = x1;
+	return d;
+}
+
 
 
 

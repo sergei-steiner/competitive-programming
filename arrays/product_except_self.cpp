41da625ab896e6091e572c2bb9ec99b93cadb793
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-25 20:16:24 +0300

    product except self

---
+++ arrays/product_except_self.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+vector<int> productExceptSelf(vector<int>& a) {
+        int n = a.size();
+        vector<int> prefix(n);
+        prefix[0] = a[0];
+        for (int i = 1; i < n; ++i) {
+            prefix[i] = prefix[i - 1] * a[i];
+        }
+        vector<int> suffix(n);
+        suffix[n - 1] = a[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffix[i] = a[i] * suffix[i + 1];
+        }
+        vector<int> res(n, 1);
+        for (int i = 0; i < n; ++i) {
+            if (i > 0) {
+                 res[i] *= prefix[i - 1];    
+            }
+            if (i + 1 < n) {
+                 res[i] *= suffix[i + 1];
+            }
+        }
+        return res;
+    }
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int x : productExceptSelf(a)) {
+        cout << x << " ";
+    }
+    return 0;
+}

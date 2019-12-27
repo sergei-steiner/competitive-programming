1b969e2cbe754b25dcbff951c5d0c76cf1d80096
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2019-12-27 14:46:08 +0300

    style

--- numbers/gcd.cpp
+++ numbers/gcd.cpp
@@ -25,7 +25,7 @@ int gcd (int a, int b, int& x, int& y) {
        return b;
     }
     int x1, y1;
-    int d = gcd (b % a, a, x1, y1);
+    int d = gcd(b % a, a, x1, y1);
     x = y1 - (b / a) * x1;
     y = x1;
     return d;

35b14377f16142591b1ddb829e5576ad674a321d
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-01-04 23:40:35 +0300

    Best Time to Buy and Sell Stock II

---
+++ arrays/stocks/buy_sell_multiple_stocks.cpp
@@ -0,0 +1,38 @@
+    
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    if (n == 0) return 0; 
+    vector<int> dp1(n);
+    vector<int> dp2(n);
+    dp1[0] = 0;
+    dp2[0] = -prices[0];
+    for (int i = 1; i < n; ++i) {
+         dp1[i] = max(dp1[i - 1], dp2[i - 1] + prices[i]);
+         dp2[i] = max(dp2[i - 1], dp1[i - 1] - prices[i]);
+    }
+    return dp1[n - 1];
+ }
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    cout << maxProfit(a) << endl;
+    return 0;
+}

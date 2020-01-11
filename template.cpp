a19727fe25ebedb6bdbaee13759573db11cc034d
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-01-11 23:16:53 +0300

    template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+    
+int main() {
+
+    return 0;
+}

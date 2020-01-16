0a46001ba4e871a4fc8402bb7109271398ebed3e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-01-17 00:49:22 +0100

    kuhn

--- graphs/kuhn.cpp
+++ graphs/kuhn.cpp
@@ -20,7 +20,7 @@ bool try_kuhn(int v) {
   }
 	used[v] = true;
 	for (int to : g[v]) {
-		if (mt[to] == -1 || try_kuh (mt[to])) {
+		if (mt[to] == -1 || try_kuhn(mt[to])) {
 			mt[to] = v;
 			return true;
 		}

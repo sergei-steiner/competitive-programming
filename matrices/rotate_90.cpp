14d4c26882aaffa6a20b0dcc1954de7b51d7ef34
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-01-19 23:29:07 +0100

    rotate matrix

---
+++ matrices/rotate_90.cpp
@@ -0,0 +1,35 @@
+    
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+
+
+// https://leetcode.com/problems/rotate-image/submissions/
+
+void rotate(vector<vector<int>>& a) {
+    int n = a.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0;  j < n; ++j) {
+            if (i > j) continue;
+            if (i >= n - j - 1) continue;
+            int val = a[i][j];
+            a[i][j] = a[n - j - 1][i];
+            a[n - j - 1][i] = a[n - i - 1][n - j - 1];
+            a[n - i - 1][n - j - 1] = a[j][n - i - 1];
+            a[j][n - i - 1] = val;
+        }
+    }
+}
+
+int main() {
+
+    return 0;
+}

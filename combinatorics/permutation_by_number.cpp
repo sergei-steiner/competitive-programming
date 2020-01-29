9c2902dd022a35e45f6d1b5e0c7e079d77e2a70d
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-01-30 01:53:50 +0300

    NumberOfPermutation

--- combinatorics/permutation_by_number.cpp
+++ combinatorics/permutation_by_number.cpp
@@ -10,11 +10,10 @@ typedef long long int64;
 using namespace std;
     
 vector<int> PermutationByNumber(int n, int k) {
-    --k;
     vector<int> f(n, 1);
     for (int i = 2; i <  n; ++i) f[i] = f[i - 1] * i;
     vector<int> ans;
-    vector<int> used(n, false);
+    vector<bool> used(n, false);
     for (int i = 0; i < n; ++i) {
         int cnt = k / f[n - i - 1];
         for (int j = 0; j < n; ++j) {
@@ -30,14 +29,35 @@ vector<int> PermutationByNumber(int n, int k) {
     }
     return ans;
 }
+
+int NumberOfPermutation(const vector<int>& p) {
+    int n = p.size();
+    vector<int> f(n, 1);
+    for (int i = 2; i <  n; ++i) f[i] = f[i - 1] * i;
+    
+    int ans = 0;
+    vector<bool> used(n, false);
+    for (int i = 0; i < n; ++i) {
+        int cnt = 0;
+        for (int j = 0; j < p[i]; ++j) {
+            if (used[j]) continue;
+            ++cnt;
+        }
+        ans += cnt * f[n - i - 1];
+        used[p[i]] = true;
+    }
+    return ans;
+}
     
 int main() {
     int n;
     cin >> n;
     int k;
     cin >> k;
-    for (int x : PermutationByNumber(n, k)) {
+    auto p = PermutationByNumber(n, k);
+    for (int x : p) {
         cout << x << " ";
     }
+    cout << NumberOfPermutation(p) << endl;
     return 0;
 }

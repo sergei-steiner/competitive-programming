ba264fd321c02b188bde73c0c89c9cff1c192f48
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-10 11:22:24 +0300

    three squares

--- numbers/sum_squares.cpp
+++ numbers/sum_squares.cpp
@@ -8,7 +8,8 @@
 typedef long long int64;
 
 using namespace std;
-    
+
+// https://en.wikipedia.org/wiki/Fermat%27s_theorem_on_sums_of_two_squares
 bool two_squares(int n) {
     for (int i = 2; i * i <= n; ++i) {
         if (n % i == 0) {
@@ -23,6 +24,15 @@ bool two_squares(int n) {
     if (n > 1 && n % 4 == 3) return false;
     return true;
 }
+
+
+// https://en.wikipedia.org/wiki/Legendre%27s_three-square_theorem
+bool three_squares(int n) {
+    
+    while (n % 4 == 0) n /= 4;
+    return n % 8 < 7;
+}
+
     
 int main() {
 

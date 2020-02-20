7ea81ce83ddb56677fd66e90fadce6a2dd2b7a4b
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-20 11:13:09 +0300

    Maximum Sum Circular Subarray

--- arrays/max_sum/kadane.cpp
+++ arrays/max_sum/kadane.cpp
@@ -43,6 +43,35 @@ pair<size_t, size_t> MaxSumRange(const vector<int>& a) {
     return {ansLeft, ansRight};
 }
 
+
+// https://leetcode.com/problems/maximum-sum-circular-subarray
+
+int maxSubarraySumCircular(vector<int>& a) {
+    int n = a.size();
+    int ans = numeric_limits<int>::min();
+    int sum = 0;
+    for (int x : a) {
+        sum += x;
+        ans = max(ans, sum);
+        if (sum < 0) sum = 0;
+    }
+    int all_sum = 0;
+    for (int x : a) all_sum += x;
+    sum = 0;
+    for (int i = 1; i < n; ++i) {
+        sum += a[i];
+        ans = max(ans, all_sum - sum);
+        if (sum > 0) sum = 0;
+    }
+    sum = 0;
+    for (int i = 0; i + 1 < n; ++i) {
+        sum += a[i];
+        ans = max(ans, all_sum - sum);
+        if (sum > 0) sum = 0;
+    }
+    return ans;
+}
+
 int main() {
 
     return 0;

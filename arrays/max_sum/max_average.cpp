a1cc70a4ac5a273c74898d42fc57cbe50a5a8922
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-24 14:03:03 +0300

    max average sum

---
+++ arrays/max_sum/max_average.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+long long MaxSum(const vector<int>& a, int minLen) {
+    long long ans = a[0];
+    int n = a.size();
+    vector<long long> prefixSums(n, 0);
+    prefixSums[0] = a[0];
+    for (int i = 1; i < n; ++i) {
+        prefixSums[i] = a[i] + prefixSums[i - 1];
+    }
+    vector<long long> minPrefixSums(n, 0);
+    minPrefixSums[0] = prefixSums[0];
+    for (size_t i = 1; i < n; ++i) {
+        minPrefixSums[i] = min(prefixSums[i], minPrefixSums[i - 1]);
+    }
+    for (int i = minLen - 1; i < n; ++i) {
+        long long result = prefixSums[i];
+        if (i - minLen > 0) result -= minPrefixSums[i - minLen - 1];
+        ans = max(ans, result);
+    }
+    return ans;
+}
+
+
+double MaxAverage(const vector<int>& a, int minLen, int iterations = 100) {
+    double minVal = *min_element(a.begin(), a.end());
+    double maxVal = *max_element(a.begin(), a.end());
+    for (int iteration = 0; iteration < iterations; ++iteration) {
+        double middle = (minVal + maxVal) / 2;
+        vector<double> b;
+        for (inx item : a) {
+            b.push_back(item - middle);
+            if (MaxSum(b, minLen) > 0) {
+                left = middle;
+            } else {
+                right = middle;
+            }
+        }
+    }
+    return left;
+}
+
+int main() {
+
+    return 0;
+}

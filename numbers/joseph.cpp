221aa61fdcd579e1bf26bf689aacc045eb4b962e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-26 11:04:50 +0300

    joseph k=2

---
+++ numbers/joseph.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+
+// k = 2
+// https://e-maxx.ru/algo/joseph_problem
+int joseph2(int n) {
+    int deg = 2;;
+    while (deg * 2 <= n) {
+        deg *= 2;
+    }        
+    return 1 + 2 * (n - deg);
+}
+
+int main() {
+
+    return 0;
+}

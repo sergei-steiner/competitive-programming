66997c14e015418cb461b905f3b46a14809a7623
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-29 21:46:24 +0300

    O(k log n)

--- numbers/joseph.cpp
+++ numbers/joseph.cpp
@@ -10,11 +10,28 @@ typedef long long int64;
 using namespace std;
 
 // https://e-maxx.ru/algo/joseph_problem
-O(n)
+
+// O(n)
 int joseph (int n, int k) {
     int res = 0;
     for (int i = 1; i <= n; ++i) res = (res + k) % i;
     return res + 1;
+}
+
+// O(k log n)
+int joseph (int n, int k) {
+    if (n == 1)  return 0;
+    if (k == 1)  return n - 1;
+    if (k > n)  return (joseph(n - 1, k) + k) % n;
+    int cnt = n / k;
+    int res = joseph(n - cnt, k);
+    res -= n % k;
+    if (res < 0)  {
+        res += n;
+    } else  {
+        res += res / (k - 1);
+    }
+    return res;
 }
 
 // k = 2

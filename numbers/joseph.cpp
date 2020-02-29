a723a0d0896081bc520de10b562809dabed1005a
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-02-29 21:47:42 +0300

    naming)

--- numbers/joseph.cpp
+++ numbers/joseph.cpp
@@ -12,14 +12,14 @@ using namespace std;
 // https://e-maxx.ru/algo/joseph_problem
 
 // O(n)
-int joseph (int n, int k) {
+int joseph_linear(int n, int k) {
     int res = 0;
     for (int i = 1; i <= n; ++i) res = (res + k) % i;
     return res + 1;
 }
 
 // O(k log n)
-int joseph (int n, int k) {
+int joseph(int n, int k) {
     if (n == 1)  return 0;
     if (k == 1)  return n - 1;
     if (k > n)  return (joseph(n - 1, k) + k) % n;

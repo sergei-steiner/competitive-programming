e7ae90ce64453661d67ac9af4fa77c5683258b21
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-02 11:42:43 +0300

    Minimum Moves to Equal Array Elements II

---
+++ arrays/make_all_equal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+ 
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+
+int minMoves2(vector<int>& a) {
+    int n = a.size();
+    if (n == 0) return 0;
+    sort(begin(a), end(a));
+    int ans = 0;
+    for (int i = 0; i < n / 2; ++i) {
+        ans += abs(a[i] - a[n - i - 1]);
+    }
+    return ans;
+}
+ 
+int main() {
+
+    return 0;
+}

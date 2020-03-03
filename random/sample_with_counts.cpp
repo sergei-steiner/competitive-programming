a29ae7973a09af536231ac1a55256abbb1088780
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-03 19:58:04 +0300

    random with counts without repetition

---
+++ random/sample_with_counts.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+vector<string> sample(const vector<pair<string, string>>& cnt, int samples) {
+    vector<pair<string, double>> a;
+    for (auto& [k, v] : cnt) {
+        a.emplace_back(log(random(0, 1)) * v, k);
+    }
+    sort(begin(a), end(a));
+    reverse(begin(a), end(a));
+    vector<string> ans;
+    for (int i = 0; i < min(n, a.size()); ++i) {
+         ans.push_back(a[i].second);
+    }
+    return ans;
+}
+    
+int main() {
+
+    return 0;
+}

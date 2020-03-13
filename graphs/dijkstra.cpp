84b8263d3b713b8eb6d5779087528897ae0d4e94
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-14 00:28:17 +0300

    dijkstra _et path

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -31,6 +31,34 @@ vector<int> dijkstra_set(int s, const vector<vector<pair<int, int>>>& g) {
     return d;
 }
 
+
+vector<int> dijkstra_set(int s, int t, const vector < vector < pair<int, int> > >& g) {
+    int n = g.size();
+    vector<int> d(n, inf);
+    vector<int> p(n, -1);
+    d[s] = 0;
+    set<pair<int, int>> q;
+    q.insert(mp(d[s], s));
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase(q.begin());
+        for (auto [to, len] : g[v]) {
+            if (d[v] + len < d[to]) {
+                q.erase(mp(d[to], to));
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.insert(mp(d[to], to));
+            }
+        }
+    }
+
+    if (d[t] == inf) return vector<int>();
+    vector<int> path;
+    for (int cur = t; cur != -1; cur = p[cur]) path.pb(cur);
+    reverse(path.begin(), path.end());
+    return path;
+}
+
 vector<int> dijkstra_heap(int s, const vector<vector<pair<int, int>>>& g) {
     int n = g.size();
     vector<int> d(n, inf);

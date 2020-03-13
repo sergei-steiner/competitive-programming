bd994b145f21a978e67b20add2da06a322f9ff8c
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-14 00:30:14 +0300

    dijkstra_heap path

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -80,6 +80,35 @@ vector<int> dijkstra_heap(int s, const vector<vector<pair<int, int>>>& g) {
     return d;
 }
 
+vector<int> dijkstra_heap(int s, int t, const vector < vector < pair<int, int> > >& g) {
+    int n = g.size();
+    vector<int> d(n, inf);
+    vector<int> p(n, -1);
+    d[s] = 0;
+    priority_queue<pair<int, int>> q;
+    q.push(mp(-d[s], s));
+    while (!q.empty()) {
+        int v = q.top().second;
+        int cur_d = -q.top().first;
+        q.pop();
+        if (cur_d > d[v]) continue;
+        for (auto [to, len] : g[v]) {
+            if (d[v] + len < d[to]) {
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.push(mp(-d[to], to));
+            }
+        }
+    }
+
+    if (d[t] == inf) return vector<int>();
+    vector<int> path;
+    for (int cur = t; cur != -1; cur = p[cur]) path.pb(cur);
+    reverse(all(path));
+    return path;
+}
+
+
 int main() {
 
     return 0;

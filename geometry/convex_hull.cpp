9db804ef0f793ad3015d9fd8a44f1c3f5c32a68e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-18 22:09:24 +0300

    style

--- geometry/convex_hull.cpp
+++ geometry/convex_hull.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <string>
-#include <math.h>
-#include <algorithm>
-#include <map>
-#include <set>
-
+#include <bits/stdc++.h>
 
 #define sz(x) ((int)x.size())
 #define all(x) (x).begin(), (x).end()
@@ -22,37 +14,37 @@ struct pt {
 };
 
 bool cmp(pt a, pt b) {
-	return a.x < b.x || a.x == b.x && a.y < b.y;
+    return a.x < b.x || a.x == b.x && a.y < b.y;
 }
 
 bool cw(pt a, pt b, pt c) {
-	return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) < 0;
+    return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) < 0;
 }
 
 bool ccw(pt a, pt b, pt c) {
-	return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) > 0;
+    return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y) > 0;
 }
 
 void convex_hull(vector<pt>& a) {
-	if (sz(a) == 1) return;
-	sort(all(a), &cmp);
-	pt p1 = a[0], p2 = a.back();
-	vector<pt> up, down;
-	up.pb(p1);
-	down.pb(p1);
-	for (int i = 1; i < sz(a); ++i) {
-		if (i == sz(a) - 1 || cw(p1, a[i], p2)) {
-			while (sz(up) >= 2 && !cw(up[sz(up) - 2], up[sz(up) - 1], a[i])) up.pop_back();
-			up.pb(a[i]);
-		}
-		if (i == sz(a) - 1 || ccw(p1, a[i], p2)) {
-			while (sz(down) >= 2 && !ccw(down[sz(down) - 2], down[sz(down) - 1], a[i])) down.pop_back();
-			down.pb(a[i]);
-		}
-	}
-	a.clear();
-	for (int i = 0; i < sz(up); ++i) a.pb(up[i]);
-	for (int i = sz(down) - 2; i > 0; --i) a.pb(down[i]);
+    if (sz(a) == 1) return;
+    sort(all(a), &cmp);
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down;
+    up.pb(p1);
+    down.pb(p1);
+    for (int i = 1; i < sz(a); ++i) {
+        if (i == sz(a) - 1 || cw(p1, a[i], p2)) {
+            while (sz(up) >= 2 && !cw(up[sz(up) - 2], up[sz(up) - 1], a[i])) up.pop_back();
+            up.pb(a[i]);
+        }
+        if (i == sz(a) - 1 || ccw(p1, a[i], p2)) {
+            while (sz(down) >= 2 && !ccw(down[sz(down) - 2], down[sz(down) - 1], a[i])) down.pop_back();
+            down.pb(a[i]);
+        }
+    }
+    a.clear();
+    for (int i = 0; i < sz(up); ++i) a.pb(up[i]);
+    for (int i = sz(down) - 2; i > 0; --i) a.pb(down[i]);
 }
 
 int main() {

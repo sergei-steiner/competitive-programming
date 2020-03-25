fe8f98df5c85fe974b00cdd697135121b6a0efb6
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-25 23:41:20 +0300

    style

--- graphs/dfs/bridges.cpp
+++ graphs/dfs/bridges.cpp
@@ -10,25 +10,24 @@ typedef long long int64;
 using namespace std;
 
 int n;
-vector<vector<int> > g;
+vector<vector<int>> g;
 vector<bool> used;
 int timer;
 vector<int> tin;
 vector<int> fup;
-vector<pair<int, int> > bridges;
+vector<pair<int, int>> bridges;
 
 void dfs(int v, int p = -1) {
     used[v] = true;
     tin[v] = fup[v] = timer++;
-    for (int i = 0; i < sz(g[v]); ++i) {
-        int to = g[v][i];
+    for (int to : g[v]) {
         if (to == p) continue;
         if (used[to]) {
             fup[v] = min(fup[v], tin[to]);
         } else {
             dfs(to, v);
             fup[v] = min(fup[v], fup[to]);
-            if (fup[to] > tin[v]) bridges.pb(mp(v, to));
+            if (fup[to] > tin[v]) bridges.push_back({v, to});
         }
     }
 }
@@ -36,9 +35,11 @@ void dfs(int v, int p = -1) {
 void find_bridges() {
     timer = 0;
     for (int i = 0; i < n; ++i) used[i] = false;
-    for (int i = 0; i < n; ++i)
-        if (!used[i])
+    for (int i = 0; i < n; ++i) {
+        if (!used[i]) {
             dfs(i);
+        }
+    }
 }
 
 

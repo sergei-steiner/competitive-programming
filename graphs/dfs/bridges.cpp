a6a984b1f6775674308ba93ec2f7a37aff9e1ba2
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-25 23:39:55 +0300

    bridges

---
+++ graphs/dfs/bridges.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+int n;
+vector<vector<int> > g;
+vector<bool> used;
+int timer;
+vector<int> tin;
+vector<int> fup;
+vector<pair<int, int> > bridges;
+
+void dfs(int v, int p = -1) {
+    used[v] = true;
+    tin[v] = fup[v] = timer++;
+    for (int i = 0; i < sz(g[v]); ++i) {
+        int to = g[v][i];
+        if (to == p) continue;
+        if (used[to]) {
+            fup[v] = min(fup[v], tin[to]);
+        } else {
+            dfs(to, v);
+            fup[v] = min(fup[v], fup[to]);
+            if (fup[to] > tin[v]) bridges.pb(mp(v, to));
+        }
+    }
+}
+
+void find_bridges() {
+    timer = 0;
+    for (int i = 0; i < n; ++i) used[i] = false;
+    for (int i = 0; i < n; ++i)
+        if (!used[i])
+            dfs(i);
+}
+
+
+int main() {
+
+    return 0;
+}

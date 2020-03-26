cf754f18c6cd1477a227379922453b59d6432e60
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-26 21:55:11 +0300

    poisson

---
+++ random/poisson.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+constexpr int N = 10000; 
+
+double uniform() {
+    return 1.0 * (rand() % (N + 1)) / N;
+}
+
+int poisson(double lambda) {
+    double L = exp(-lambda);
+    double p = 1;
+    int k = 0;
+    do {
+        ++k;
+        p *= uniform01();
+    } while (p > L);
+    return k - 1;
+}
+    
+int main() {
+
+    return 0;
+}

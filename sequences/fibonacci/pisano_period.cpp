46f34d7b6cf2f1c88e43068f713568c32983ce28
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-28 17:54:18 +0300

    while true

--- sequences/fibonacci/pisano_period.cpp
+++ sequences/fibonacci/pisano_period.cpp
@@ -8,17 +8,20 @@
 typedef long long int64;
 
 using namespace std;
+
+// https://en.wikipedia.org/wiki/Pisano_period
     
+//  linear algo
 int pisano_period_naive(int n) {
     int x = 0;
     int y = 1 % n;
-    int ans = 1;
-    for (int i = 0; i < n; ++i) {
-        if (x == 0) return ans;
-        ++ans;
+    int ans = 0;
+    while (true) {
         int z = (x + y) % n;
         x = y;
         y = z;
+        ++ans;
+        if (x == 0) return ans;
     }
     return -1;
 }

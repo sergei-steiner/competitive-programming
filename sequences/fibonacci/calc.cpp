f5d11470607cdd974f1705a79ae43d0135955e87
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-03-29 23:49:59 +0300

    debug

--- sequences/fibonacci/calc.cpp
+++ sequences/fibonacci/calc.cpp
@@ -19,17 +19,18 @@ Matrix mulmod(const Matrix& a, const Matrix& b) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             for (int k = 0; k < n; ++k) {
-                 c[i][j] += a[i][k] * b[k][j];
+                 c[i][j] += (long long)a[i][k] * b[k][j] % MODULO;
                  c[i][j] %= MODULO;
             }
         }
     }
+	return c;
 }
 
 Matrix powmod(const Matrix& a, int n) {
     if (n == 0) {
-        Matrix E(n, vector<int>(n, 0));
-        for (int i = 0; i < n; ++i) E[i][i] = 1;
+        Matrix E(a.size(), vector<int>(a.size(), 0));
+        for (int i = 0; i < a.size(); ++i) E[i][i] = 1;
         return E;
     }
     if (n % 2 == 1) return mulmod(a, powmod(a, n - 1));
@@ -41,7 +42,7 @@ int fib(int n) {
      Matrix E(2, vector<int>(2, 0));
      E[0] = {0, 1};
      E[1] = {1, 1};
-     Matrix M = powmod(E, n);
+	 Matrix M = powmod(E, n);
      return (M[0][0] + M[0][1]) % MODULO;
 }
     

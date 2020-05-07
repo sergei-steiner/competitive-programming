76b3e81b7436e89f71e396a17e9e0981dc05c65e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-05-08 00:15:18 +0300

    Update lis.cpp

--- arrays/lis.cpp
+++ arrays/lis.cpp
@@ -27,6 +27,37 @@ int lengthOfLIS(vector<int>& a) {
          }
     }
 }
+
+vector<int> LIS(vector<int>& a) {
+    int n = a.size();
+    vector<int> d(n + 1, numeric_limits<int>::max());
+    vector<int> ind(n + 1, -1);
+    vector<int> p(n, -1);
+    
+    d[0]  = numeric_limits<int>::min();
+    for (int j = 0; j < n; ++j) {
+        int x = a[j];
+        int i = upper_bound(d.begin(), d.end(), x) - d.begin();
+        if (d[i - 1] != x) {
+            d[i] = x;
+            ind[i] = j;
+            p[j] = ind[i - 1];
+            if (i + 1 <= n) p[ind[i + 1]] = ind[i];
+        }
+    }
+    for (int i = n; i >= 0; --i) {
+         if (d[i] < numeric_limits<int>::max()) {
+             int j = ind[i];
+             vector<int> ans;
+             while (j != -1) {
+                 ans.push_back(j);
+                 j = p[j];
+             }
+             reverse(begin(ans) end(ans));
+             return ans;
+         }
+    }
+}
     
 int main() {
     int n;

913e5ef210036814201fb93e833eb021b1ff576b
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-01 14:12:52 +0300

    Create c_n_k.cpp

---
+++ combinatorics/combinations/c_n_k.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// note 1: for probabilities it is sometimes helpful to use double instead of int64
+// note 2: unordeder_map is faster, but no support for pairs: implement hash function by yourself
+map<pair<int, int>, int64> C_dp;
+int64 C(int n, int k) {
+    if (k < 0) return 0;
+    if (k > n) return 0;
+    if (n == 0) return 1; // k = 0
+    if (C_dp.count({n, k})) return C_dp[{n, k}];
+    int64 ans = C(n - 1, k - 1) + C(n - 1, k);
+    C_dp[{n, k}] = ans;
+    return ans;
+}
+    
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << C(n, k) << endl;
+
+    return 0;
+}

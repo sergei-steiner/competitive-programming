7fa4e9552a88320449c6cbcb73e52f540cf1c302
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-05 16:03:43 +0300

    Update and rename gray_int.cpp to antigray.cpp

---
+++ combinatorics/gray_code/antigray.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+vector<vector<int>> MirrorGray(int n) {
+    if (n == 1) {
+        return {{0}, {1}};
+    }
+    auto g = MirrorGray(n - 1);
+    vector<vector<int>> ans;
+    for (auto& v : g) {
+       v.emplace_back(0);
+       ans.emplace_back(v);
+    }
+    reverse(begin(g), end(g);
+    for (auto v : g) {
+       v.emplace_back(1);
+       ans.emplace_back(v);
+    }
+    return ans;
+}
+	    
+vector<int> inv(const vector<int>& v) {
+    vector<int> ans;
+    for (int x : v) {
+        ans.push_back(1 - x);
+    }
+    return ans;
+}
+
+vector<vector<int>> AntiGray(int n) {
+    if (n == 1) {
+        return {{0}, {1}};
+    }
+    auto g = MirrorGray(n - 1);
+    vector<vector<int>> ans;
+    for (auto v : g) {
+       v.emplace_back(0);
+       ans.emplace_back(v);
+       ans.emplace_back(inv(v));
+    }
+    return ans;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    cout << n << " -> " << gray(n) << " -> " << reverse_gray(gray(n)) << endl;
+    
+    return 0;
+}

69871f43703943badf7189737e0aa684b0a83bff
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-10 17:18:36 +0300

    Create linear-sieve.cpp

---
+++ numbers/primes/linear-sieve.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// http://e-maxx.ru/algo/prime_sieve_linear
+
+vector<int> linear_sieve(int n) {
+    vector<int> lp(n + 1, 0);
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+	    if (lp[i] == 0) {
+		    lp[i] = i;
+		    primes.pb(i);
+	    }
+	    for (int j = 0; j < sz(primes) && primes[j] <= lp[i] && i * primes[j] <= n; ++j)
+            lp[i * primes[j]] = primes[j];
+    }
+    return primes;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    auto primes = linear_sieve(n);
+    for (int p : primes) {
+        cout << p << endl;
+    }
+    
+    return 0;
+}

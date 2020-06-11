4771c54bf19b64d7512546cbd7718a484beea2bd
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-11 16:08:57 +0300

    Update dutch_flag.cpp

--- two_pointers/dutch_flag.cpp
+++ two_pointers/dutch_flag.cpp
@@ -8,25 +8,33 @@
 typedef long long int64;
 
 using namespace std;
-    
-void threeWayPartition(const vector<int>& a, int mid) {
-    int n = a.size();
-    int i = 0;
-    int j = 0;
-    int k = n;
-    while (j < k) {
-      if (a[j] < mid) {
-            swap(a[i], a[j]);
-            ++i;
-            ++j;
-        } else if (a[j] > mid) {
-            --k;
-            swap(a[j], a[k]);
-        } else {
-            ++j;
+
+// https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+// https://leetcode.com/problems/sort-colors
+class Solution {
+public:
+    void threeWayPartition(vector<int>& a, int mid) {
+        int n = a.size();
+        int i = 0;
+        int j = 0;
+        int k = n;
+        while (j < k) {
+          if (a[j] < mid) {
+                swap(a[i], a[j]);
+                ++i;
+                ++j;
+            } else if (a[j] > mid) {
+                --k;
+                swap(a[j], a[k]);
+            } else {
+                ++j;
+            }
         }
     }
-}
+    void sortColors(vector<int>& a) {
+        threeWayPartition(a, 1);
+    }
+};
     
 int main() {
 

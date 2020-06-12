2319025fdefbdfb9195b12d2d8badb2549aaa443
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-12 18:05:54 +0300

    Update egg-dropping.cpp

--- combinatorics/adhoc/egg-dropping.cpp
+++ combinatorics/adhoc/egg-dropping.cpp
@@ -9,51 +9,44 @@ typedef long long int64;
 
 using namespace std;
 
-map<pair<int, int>, int> dp;
-    
-int C(int n, int k) {
-    if (n < k) return 0;
- 
-    if (n == 0) return 1;
-    if (k == 0) return 1;
-        
-    auto it = dp.find({n, k});
-    if (it != dp.end()) return it->second;
-    dp[{n, k}] = C(n - 1, k - 1) + C(n - 1, k);
-    return dp[{n, k}];
-}
-
 // https://brilliant.org/wiki/egg-dropping/
 // https://leetcode.com/problems/super-egg-drop/
+// O(K log N) without taking C(n, k) into account
 
-  
-int superEggDrop(int K, int N) {
-    int left = 0;
-    int right = N;
-    while(right - left > 1) {
-        int d = left + (right - left) / 2;
-        int sum = 0;
-        for (int i = 1; i <= K; ++i) {
-            sum += C(d, i);
-            if (sum >= N) break;
-        }
-        if (sum < N) {
-            left = d;
-        } else {
-            right = d;
-        }
+class Solution {
+public:
+    map<pair<int, int>, int> C_dp;
+    int C(int n, int k) {
+        if (k < 0) return 0;
+        if (k > n) return 0;
+        if (n == 0) return 1; // k = 0
+        if (C_dp.count({n, k})) return C_dp[{n, k}];
+        int ans = C(n - 1, k - 1) + C(n - 1, k);
+        C_dp[{n, k}] = ans;
+        return ans;
     }
-    return left + 1;
- }
-
+    
+    int superEggDrop(int K, int N) {
+        int left = 0;
+        int right = N;
+        while(right - left > 1) {
+            int d = left + (right - left) / 2;
+            int sum = 0;
+            for (int i = 1; i <= K; ++i) {
+                sum += C(d, i);
+                if (sum >= N) break;
+            }
+            if (sum < N) {
+                left = d;
+            } else {
+                right = d;
+            }
+        }
+        return left + 1;
+    }
+};
 
 int main() {
-    int n;
-    cin >> n;
-    int k;
-    cin >> k;
-    
-    cout << superEggDrop(k, n) << endl;
     
     return 0;
 }

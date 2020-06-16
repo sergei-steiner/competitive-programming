425b2cd6b1ccc72639cff0711c67fdc93cb1399b
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-16 15:17:52 +0300

    Update bellman-ford.cpp

--- graphs/shortest-path/bellman-ford/bellman-ford.cpp
+++ graphs/shortest-path/bellman-ford/bellman-ford.cpp
@@ -32,6 +32,49 @@ vector<int> bellman_ford(int n, const vector<edge>& edges, int s) {
     return d;
 }
 
+vector<int> bellman_ford_refined(int n, const vector<edge>& edges, int s) {
+    vector<int> d(n, inf);
+    d[s] = 0;
+    while (true) {
+        bool any = false;
+        for (auto e : edges) {
+            if (d[e.x] < inf) {
+                d[e.y] = min(d[e.y], d[e.x] + e.cost);
+                any = true;
+            }
+        }
+        if (!any) break;
+    }
+    return d;
+}
+
+vector<int> bellman_ford_refined(int n, const vector<edge>& edges, int s, int t) {
+    vector<int> d(n, inf);
+    vector<int> p(n, -1);
+    d[s] = 0;
+    while (true) {
+        bool any = false;
+        for (auto e : edges) {
+            if (d[e.x] < inf) {
+                if (d[e.y] < d[e.x] + e.cost) {
+                    d[e.y] = d[e.x] + e.cost;
+                    p[e.y] = e.x;
+                }
+                any = true;
+            }
+        }
+        if (!any) break;
+    }
+    if (d[t] > inf / 2) return {};
+    vector<int> ans;
+    for (int cur = t; cur != s; cur = p[cur]) {
+        ans.push_back(cur);
+    }
+    reverse(begin(ans), end(ans));
+    return ans;
+}
+
+
 int main() {
    
 }

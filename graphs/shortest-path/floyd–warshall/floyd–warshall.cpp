ad2c10e59ae5cfbf2ead38103bd7ea7e6c4f3c9e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-18 23:26:13 +0300

    Create floyd–warshall.cpp

---
+++ graphs/shortest-path/floyd–warshall/floyd–warshall.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+ 
+vector<vector<int>> floyd(const vector<vector<int>>& a) {
+    vector<vector<int>> d = a;
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                 d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+    return d;
+}
+ 
+int main() {
+
+    return 0;
+}

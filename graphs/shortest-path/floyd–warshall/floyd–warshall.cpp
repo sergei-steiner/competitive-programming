922fe3f1c029b587df93dac25e98413cf5049cec
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-18 23:30:17 +0300

    Update floyd–warshall.cpp

--- graphs/shortest-path/floyd–warshall/floyd–warshall.cpp
+++ graphs/shortest-path/floyd–warshall/floyd–warshall.cpp
@@ -8,12 +8,15 @@
 typedef long long int64;
 
 using namespace std;
+
+// http://e-maxx.ru/algo/floyd_warshall_algorithm
  
 vector<vector<int>> floyd(const vector<vector<int>>& a) {
     vector<vector<int>> d = a;
     for (int k = 0; k < n; ++k) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
+                 // if (d[i][k] < inf && d[k][j] < inf) to cope with negative cycles
                  d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
             }
         }

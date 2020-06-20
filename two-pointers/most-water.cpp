e7bdb98e2a5e7a4dc773bec7767dded8b6c126cc
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-20 20:35:59 +0300

    Update most-water.cpp

--- two-pointers/most-water.cpp
+++ two-pointers/most-water.cpp
@@ -12,29 +12,25 @@ using namespace std;
 
 // https://leetcode.com/problems/container-with-most-water/
 
-int maxArea(vector<int>& a) {
-    int ans = 0;
-    int i = 0;
-    int j = a.size() - 1;
-    while (i < j) {
-        ans = max(ans, min(a[i], a[j]) * (j - i));
-        if (a[i] < a[j]) {
-            ++i;
-        } else {
-            --j;
+class Solution {
+public:
+    int maxArea(vector<int>& a) {
+        int ans = 0;
+        int i = 0;
+        int j = a.size() - 1;
+        while (i < j) {
+            ans = max(ans, min(a[i], a[j]) * (j - i));
+            if (a[i] < a[j]) {
+                ++i;
+            } else {
+                --j;
+            }
         }
-     }
-     return ans;
-}
+        return ans;
+    }
+};
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-    }
-    cout << maxArea(a) << endl;
-
+   
     return 0;
 }

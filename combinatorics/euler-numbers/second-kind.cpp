6a9464d291934e1af17bd188fecd592c26a39d7f
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-06-27 23:31:20 +0300

    Update second-kind.cpp

--- combinatorics/euler-numbers/second-kind.cpp
+++ combinatorics/euler-numbers/second-kind.cpp
@@ -10,17 +10,18 @@ typedef long long int64;
 using namespace std;
 
 // https://neerc.ifmo.ru/wiki/index.php?title=Числа_Эйлера_I_и_II_рода
-map<pair<int, int>, int> A_dp;
-int A(int n, int m) {
+map<pair<int, int>, int> a_dp;
+int a(int n, int m) {
    if (n == 0 && m == 0) return 1;
    if (n == 0) return 0;
    if (m == 0) return 0;
-   if (m >= n) return 0;
-   if (A_dp.count({n, m})) {
-       return A_dp[{n, m}];
+   if (m >= 2 * n - 1) return 0;
+   if (a_dp.count({n, m})) {
+       return a_dp[{n, m}];
    }
-   int ans = (n - m) * A(n - 1, m - 1) + (m + 1) * A(n - 1, m);
-   A_dp[{n, m}] = ans;
+   int ans = (2 * n - m - 1) * a(n - 1, m - 1) + (m + 1) * a(n - 1, m);
+   a_dp[{n, m}] = ans;
+   return ans;
 }
     
 int main() {

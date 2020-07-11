541922c984718d0473f5abe599af1aadc681eaea
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-07-11 19:39:56 +0400

    Update kruskal.cpp

--- graphs/minimum-spanning-tree/kruskal.cpp
+++ graphs/minimum-spanning-tree/kruskal.cpp
@@ -9,56 +9,58 @@ typedef long long int64;
 
 using namespace std;
 
+// https://leetcode.com/problems/connecting-cities-with-minimum-cost
 // https://neerc.ifmo.ru/wiki/index.php?title=Алгоритм_Краскала
 // O(ElogE)
 
-int n;
-vector<int> p;
+class Solution {
+public:
 
-int findset(int v) {
-    if (v == p[v]) return v;
-    return p[v] = findset(p[v]);
-}
+    int n;
+    vector<int> p;
 
-void unionsets(int u, int v) {
-    u = findset(u);
-    v = findset(v);
-    if (u == v) return;
-    if (rand() & 1) swap(u, v);
-    p[u] = v;
- }
- 
- int spanning_tree_length(vector<vector<int>> e) {
-    int ans = 0;
-    p.assign(n, 0);
-    for (int i = 0; i < n; ++i) p[i] = i;
-    sort(begin(e), end(e), [](auto x, auto y){ return x[2] < y[2]; });
-    for (auto edge : e) {
-       int x = edge[0];
-       int y = edge[1];
-       int w = edge[2];
-       if (findset(x) == findset(y)) continue;
-       ans += w;
-       unionsets(x, y);
+    int findset(int v) {
+        if (v == p[v]) return v;
+        return p[v] = findset(p[v]);
     }
-    return ans;
-  }
+
+    void unionsets(int u, int v) {
+        u = findset(u);
+        v = findset(v);
+        if (u == v) return;
+        if (rand() & 1) swap(u, v);
+        p[u] = v;
+     }
+
+     int spanning_tree_length(int n, vector<vector<int>> e) {
+        int ans = 0;
+        p.assign(n, 0);
+        for (int i = 0; i < n; ++i) p[i] = i;
+        sort(begin(e), end(e), [](auto x, auto y){ return x[2] < y[2]; });
+        for (auto edge : e) {
+           int x = edge[0] - 1;
+           int y = edge[1] - 1;
+           int w = edge[2];
+           if (findset(x) == findset(y)) continue;
+           ans += w;
+           unionsets(x, y);
+         }
+         for (int i = 0; i < n; ++i) {
+             if (findset(0) != findset(i)) {
+                 return -1;
+             }
+         }
+         return ans;
+      }
+    
+    
+    int minimumCost(int N, vector<vector<int>>& connections) {
+        return spanning_tree_length(N, connections);
+    }
+};
     
     
 int main() {
-    cin >> n;
-    int m;
-    cin >> m;
-    vector<vector<int>> e;
-    for (int i = 0; i < m; ++i) {
-        int x;
-        cin >> x;
-        int y;
-        cin >> y;
-        int w;
-        cin >> w;
-        e.push_back({x, y, w});
-    }
-    cout << spanning_tree_length(e) << endl;
+ 
     return 0;
 }

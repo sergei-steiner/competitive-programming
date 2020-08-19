9cc09004d21ce518de57c44aed84e365678548a3
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-08-20 00:34:02 +0400

    Create remove-1-to-n-stones-from-pile.cpp

---
+++ games/symmetry/remove-1-to-n-stones-from-pile.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// https://leetcode.com/problems/nim-game
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+        
+    }
+};
+
+int main() {
+
+    return 0;
+}

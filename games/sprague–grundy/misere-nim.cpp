cee9942cd7a66d46f051bc3482e96094d0233b75
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-09-02 22:07:50 +0400

    Update misere-nim.cpp

--- games/sprague–grundy/misere-nim.cpp
+++ games/sprague–grundy/misere-nim.cpp
@@ -27,6 +27,44 @@ bool canWin(const vector<int>& a) {
     return ans != 0;
 }
 
+bool all_ones(const vector<int>& a) {
+    for (int x : a) {
+        if (x > 1) return false;
+    }
+    return true;
+}
+
+void move(vector<int>& a) {
+    int ans = 0;
+    for (auto x : a) ans ^= x;
+    
+    if (ans == 0) {
+        for (auto& x : a) {
+            if (x != 0) {
+               --x;
+                return;
+            }
+        }
+    }
+    
+    int j = -1;
+    for (int i = 31; i >= 0; --i) {
+        if (ans & (1 << i)) {
+            j = i;
+            break;
+        }
+    }
+    for (auto& x : a) {
+        if (x & (1 << j)) {
+            x = x ^ ans;
+            if (all_ones(a)) {
+               x = 0;
+            }
+            return;
+        }
+    }
+}
+
 int main() {
 
     return 0;

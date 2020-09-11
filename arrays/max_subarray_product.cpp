f1b4e251362fc7c452788742cb8e5a480258dd73
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-09-11 22:41:04 +0400

    Update max_subarray_product.cpp

--- arrays/max_subarray_product.cpp
+++ arrays/max_subarray_product.cpp
@@ -51,6 +51,42 @@ public:
         return ans;
     }
 };
+
+class Solution2 {
+public:
+    long long solve_no_zeros(vector<int> a) {
+        if (a.size() == 1) return a[0];
+        long long ans = 1;
+        for (int x : a) ans *= x;
+        if (ans > 0) return ans;
+        long long ans1 = ans;
+        for (int i = 0; i + 1 < a.size(); ++i) {
+            ans1 /= a[i];
+            if (ans1 > 0) break;
+        }
+        long long ans2 = ans;
+        for (int i = a.size() - 1; i > 0; --i) {
+            ans2 /= a[i];
+            if (ans2 > 0) break;
+        }
+        return max(ans1, ans2);
+    }
+    int maxProduct(vector<int>& b) {
+        int n = b.size();
+        long long ans = numeric_limits<long long>::min();
+        vector<int> a;
+        for (int i = 0; i <= n; ++i) {
+            if (i == b.size() || b[i] == 0) {                
+                if (i < b.size()) ans = max(ans, (long long) 0);
+                if (a.size() > 0) ans = max(ans, solve_no_zeros(a));
+                a.clear();
+                continue;
+            }
+            a.push_back(b[i]);
+        }
+        return ans;
+    }
+};
     
 int main() {
 

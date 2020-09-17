255d58e2531d1552061ce0414ae1f45df3b304bf
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-09-17 21:45:53 +0400

    Create chicken-mcnuggets-theorem.cpp

---
+++ combinatorics/adhoc/chicken-mcnuggets-theorem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// https://artofproblemsolving.com/wiki/index.php/Chicken_McNugget_Theorem
+
+int chicken_mcnuggets_value(int m, int n) {
+    return m * n - m - n;
+}
+    
+int main() {
+
+    return 0;
+}

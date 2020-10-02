a0bbc4d1a9986165db06b86e667a97803edb8cfb
sergei.shteiner <sergei.shteiner@deliveryhero.com>
2020-10-02 15:42:05 +0400

    dp, over subsegments, lines of wines, solutions 2, 3

--- dp/over-subsegments/line-of-wines.cpp
+++ dp/over-subsegments/line-of-wines.cpp
@@ -36,14 +36,50 @@ int solution1(const vector<int>& p) {
     return dp[0][n - 1];
 }
 
+// top-down
+// "push" dp
+
 int solution2(const vector<int>& p) {
     int n = p.size();
-    return 0;
+    // dp[i][j] - how much we can earn so far
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int len = n; len >= 2; --len) {
+        for (int i = 0; i + len <= n; ++i) {
+            int j = i + len - 1;
+            int year = i + n - j;
+            // push
+            dp[i + 1][j] = max(dp[i + 1][j], year * p[i] + dp[i][j]);
+            dp[i][j - 1] = max(dp[i][j - 1], year * p[j] + dp[i][j]);
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans = max(ans, dp[i][i] + n * p[i]);
+    }
+    return ans;
 }
+
+// bottom-up
+// problem-specific
 
 int solution3(const vector<int>& p) {
     int n = p.size();
-    return 0;
+    vector<int> prefix = p;
+    for (int i = 1; i < n; ++i) {
+        prefix[i] += prefix[i - 1];
+    }
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) dp[i][i] = p[i];
+    for (int len = 2; len <= n; ++len) {
+        for (int i = 0; i + len <= n; ++i) {
+            int j = i + len - 1;
+            int sum = prefix[j];
+            if (i > 0) sum -= prefix[i - 1];
+            dp[i][j] = sum + max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+     
+    return dp[0][n - 1];
 }
 
 int main() {

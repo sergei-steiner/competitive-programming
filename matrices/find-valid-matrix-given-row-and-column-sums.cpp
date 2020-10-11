66ddeda1e93bc0808ee7174e1578772cca28fb25
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-10-11 13:23:20 +0400

    Update find-valid-matrix-given-row-and-column-sums.cpp

--- matrices/find-valid-matrix-given-row-and-column-sums.cpp
+++ matrices/find-valid-matrix-given-row-and-column-sums.cpp
@@ -59,6 +59,25 @@ public:
         return a;
     }
 };
+
+// the intended solution O(n ^ 2)
+
+class Solution2 {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n = rowSum.size();
+        int m = colSum.size();
+        vector<vector<int>> a(n, vector<int>(m, 0));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                a[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= a[i][j];
+                colSum[j] -= a[i][j];
+            }
+        }
+        return a;
+    }
+};
     
 int main() {
 

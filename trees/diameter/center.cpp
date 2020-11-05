d108a007b1c800c86938ba60a9281fb8064b535e
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-11-05 21:37:14 +0400

    Update center.cpp

--- trees/diameter/center.cpp
+++ trees/diameter/center.cpp
@@ -12,6 +12,7 @@ using namespace std;
 // https://leetcode.com/problems/minimum-height-trees/
 
 // via diameter
+// O(n)
 
 class Solution {
 public:
@@ -72,6 +73,54 @@ public:
             return { path[m / 2] };
         }
         return {path[m / 2], path[m / 2 - 1]};
+    }
+};
+
+
+// erasing leaves
+// O(n)
+
+class Solution2 {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return {0};
+        vector<vector<int>> g(n);
+        for (auto e : edges) {
+            int x = e[0];
+            int y = e[1];
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+        vector<int> leaves;
+        vector<int> degree(n, 0);
+        for (int i = 0; i < n; ++i) {
+            degree[i] = g[i].size();
+            if (degree[i] == 1) {
+                leaves.push_back(i);
+            }
+        }
+        vector<bool> erased(n, false);
+        int left = n - leaves.size();
+        if (left == 0) return leaves;
+        while (true) {
+            vector<int> new_leaves;
+            for (auto v : leaves) {
+                erased[v] = true;
+                for (auto to : g[v]) {
+                    if (!erased[to]) {
+                        --degree[to];
+                        if (degree[to] == 1) {
+                            new_leaves.push_back(to);
+                        }
+                    }
+                }
+            }
+            swap(leaves, new_leaves);
+            if (left <= 2) break;
+            left -= leaves.size();
+        }
+        
+        return leaves;
     }
 };
     

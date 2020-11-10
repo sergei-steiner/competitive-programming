e494dee83963bfe79a2c4e924dbd1125ab214e7a
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-11-10 15:34:06 +0400

    Create count-less-than-x.cpp

---
+++ data-structures/fenwick/count-less-than-x.cpp
@@ -0,0 +1,64 @@
+include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+struct Fenwick {
+    int n = 0;
+    vector<int> t;
+
+    Fenwick(int n)
+        : n(n)
+        , t(vector<int>(n, 0))
+    {
+    }
+
+    int sum(int r) {
+        int result = 0;
+        while (r >= 0) {
+            result += t[r];
+            r = (r & (r + 1)) - 1;
+        }
+        return result;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void inc(int i, int delta) {
+        while (i < n) {
+            t[i] += delta;
+            i |= (i + 1);
+        }
+    }
+};
+
+// https://leetcode.com/problems/create-sorted-array-through-instructions
+
+class Solution {
+public:
+    int createSortedArray(vector<int>& instructions) {
+        int N = 100000;
+        Fenwick f(N + 1);
+        long long ans = 0;
+        int cnt = 0;
+        for (int x : instructions) {
+            ans += min(f.sum(x - 1), f.sum(x + 1, N));
+            f.inc(x, 1);
+        }
+        int mod = 1000000007;
+        return ans % mod;
+    }
+};
+    
+int main() {
+
+    return 0;
+}

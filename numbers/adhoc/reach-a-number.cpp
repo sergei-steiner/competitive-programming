eec9f5ab61843c9ace75861291ad9c1eba37e8d0
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2020-12-28 13:11:27 +0400

    Create reach-a-number.cpp

---
+++ numbers/adhoc/reach-a-number.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+
+typedef long long int64;
+
+using namespace std;
+
+// https://leetcode.com/problems/reach-a-number/
+
+// O(sqrt(n))
+// could be easily done in O(1)
+
+class Solution {
+public:
+    int reachNumber(int target) {
+        if (target < 0) target *= -1;
+        long long n  = 0;
+        while (n * (n + 1) / 2 < target) {
+            ++n;
+        }
+        while ((n * (n + 1) / 2) % 2 != target % 2) ++n;
+        return n;
+        
+    }
+};
+    
+int main() {
+
+    return 0;
+}

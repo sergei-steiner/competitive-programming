c01e3bc3328c04b90cd6195078b30325b63c89fe
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2021-01-11 12:11:25 +0400

    Create merge-sorted-array-inplace.cpp

---
+++ sorting/merge-sort/merge-sorted-array-inplace.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+typedef long long int64;
+
+using namespace std;
+
+// https://leetcode.com/problems/merge-sorted-array
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        for (int k = n + m - 1; k >= 0; --k) {
+            if (i < 0) {
+                nums1[k] = nums2[j];
+                --j;
+                continue;
+            }
+            if (j < 0) {
+                nums1[k] = nums1[i];
+                --i;
+                continue;
+            }
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                --i;
+                continue;
+            }
+            nums1[k] = nums2[j];
+            --j;
+        }
+    }
+};
+    
+int main() {
+
+    return 0;
+}

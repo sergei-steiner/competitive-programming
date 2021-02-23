2528e1e750cbf65fa37fe5225aeb81b432767bcb
sergei-steiner <39887059+sergei-steiner@users.noreply.github.com>
2021-02-23 10:19:35 +0100

    Update guess-the-majority.cpp

--- balance-puzzles/guess-the-majority.cpp
+++ balance-puzzles/guess-the-majority.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 // https://leetcode.com/problems/guess-the-majority-in-a-hidden-array
 
-// my solution \theta(N)
-// just find majority along first 5
-// then query every other
-
 /**
  * // This is the ArrayReader's API interface.
  * // You should not implement it, or speculate about its implementation
@@ -25,6 +21,10 @@ using namespace std;
  *     int length();
  * };
  */
+
+// my solution \Theta(N)
+// just find majority along first 5
+// then query every other
 
 class Solution {
 public:
@@ -100,6 +100,108 @@ public:
         }
         for (int i = 0; i < n; ++i) {
             if (val[i] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+// Theta(n / 2) approach
+
+// again, find majority
+// but then query the rest in pairs
+// cause only balance is important
+
+class Solution2 {
+public:
+    int query(ArrayReader &reader, int x, int y, int z, int w) {
+        vector<int> a = {x, y, z, w};
+        sort(begin(a), end(a));
+        return reader.query(a[0], a[1], a[2], a[3]);
+    }
+    int guessMajority(ArrayReader &reader) {
+        int n = reader.length();
+        vector<int> a;
+        a.push_back(reader.query(1, 2, 3, 4));
+        a.push_back(reader.query(0, 2, 3, 4));
+        a.push_back(reader.query(0, 1, 3, 4));
+        a.push_back(reader.query(0, 1, 2, 4));
+        a.push_back(reader.query(0, 1, 2, 3));
+
+        int x = -1;
+        int y = -1;
+        int z = -1;
+        
+        for (int i = 0; i < 5; ++i) {
+            if (a[i] == 4) {
+                vector<int> rest;
+                for (int j = 0; j < 5; ++j) {
+                    if (i == j) continue;
+                    rest.push_back(j);
+                }
+                x = rest[0];
+                y = rest[1];
+                z = rest[2];
+            }
+        }
+        
+        if (x == -1)
+        for (int i = 0; i < 5; ++i) {
+            if (a[i] == 2) {
+                for (int j = 0; j < 5; ++j) {
+                    if (i != j && a[j] == 2) {
+                        vector<int> rest;
+                        for (int k = 0; k < 5; ++k) {
+                            if (k == i || k == j) continue;
+                            rest.push_back(k);
+                        }
+                        x = rest[0];
+                        y = rest[1];
+                        z = rest[2];
+                    }
+                }
+            }
+        }
+        int zeros = 2;
+        int ones = 0;
+        vector<int> left;
+        for (int i = 0; i < n; ++i) {
+            if (i == x) continue;
+            if (i == y) continue;
+            left.push_back(i);
+        }
+        int one_index = -1;
+        for (int i = 0; i < left.size() / 2; ++i) {
+            int result = query(reader, x, y, left[2 * i], left[2 * i + 1]);
+            if (result == 0) {
+                ones += 2;
+                one_index = left[2 * i];
+            }
+            if (result == 2) {
+                zeros += 1;
+                ones += 1;
+            }
+            if (result == 4) {
+                zeros += 2;
+            }
+        }
+        if (left.size() % 2 == 1) {
+            int i = left.back();
+            if (left.back() == z) {
+                ++zeros;
+            } else {
+                int result = query(reader, x, y, z, left.back());
+                if (result == 4) {
+                    ++zeros;
+                } else {
+                    ++ones;
+                }
+            }
+        }
+        if (zeros > ones) {
+            return x;
+        }
+        if (ones > zeros) {
+            return one_index;
         }
         return -1;
     }
